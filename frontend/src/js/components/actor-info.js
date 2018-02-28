391f292e6f4727d5c0f1226194596f39661dce56
mikhailmatveev <mikhailmatveev.tmb@gmail.com>
2018-02-28 21:32:51 +0300

    actor-info markup and styles refactored

--- frontend/src/js/components/actor-info.js
+++ frontend/src/js/components/actor-info.js
@@ -3,36 +3,38 @@ import axios from 'axios';
 module.exports.ActorInfo = Vue.component('actor-info', {
     template: `<div class="component-wrapper">
         <div class="card">
-            <div class="img-wrapper">
-                <img :src="actor.avatars[0]" :alt="actor.name" class="img-responsive img-thumbnail"/>
-            </div>
             <div class="card-body">
-                <h4>{{actor.name}}</h4>
-                <ul>
-                    <li v-if="hasAliases()">
-                        <span class="text-muted">Так же известен как</span>
-                        <span>{{getAliases()}}</span>
-                    </li>
-                    <li><span class="text-muted">Страна</span><span>{{actor.country}}</span></li>
-                    <li><span class="text-muted">Дата рождения</span><span>{{actor.birthDate}}</span></li>
-                    <li><span class="text-muted">Место рождения</span><span>{{actor.birthPlace}}</span>
-                    </li>
-                    <li v-if="actor.status">
-                        <span class="text-muted">Карьерный статус</span>
-                        <span>{{actor.status}}</span>
-                    </li>
-                    <li><span class="text-muted">Рост</span><span>{{actor.height}}</span></li>
-                    <li v-if="actor.color">
-                        <span class="text-muted">Цвет глаз</span>
-                        <span>{{actor.color}}</span>
-                    </li>
-                    <li v-if="hasLinks()">
-                        <span class="text-muted">Страницы</span>
-                        <ul class="links">
-                            <li v-for="link in actor.links"><a :href="link">{{link}}</a></li>
-                        </ul>
-                    </li>
-                </ul>
+                <div class="avatar">
+                    <img :src="actor.avatars[0]" :alt="actor.name" class="img-responsive img-thumbnail"/>
+                </div>
+                <div class="info">
+                    <h4>{{actor.name}}</h4>
+                    <ul>
+                        <li v-if="hasAliases()">
+                            <span class="text-muted">Так же известен как</span>
+                            <span>{{getAliases()}}</span>
+                        </li>
+                        <li><span class="text-muted">Страна</span><span>{{actor.country}}</span></li>
+                        <li><span class="text-muted">Дата рождения</span><span>{{actor.birthDate}}</span></li>
+                        <li><span class="text-muted">Место рождения</span><span>{{actor.birthPlace}}</span>
+                        </li>
+                        <li v-if="actor.status">
+                            <span class="text-muted">Карьерный статус</span>
+                            <span>{{actor.status}}</span>
+                        </li>
+                        <li><span class="text-muted">Рост</span><span>{{actor.height}}</span></li>
+                        <li v-if="actor.color">
+                            <span class="text-muted">Цвет глаз</span>
+                            <span>{{actor.color}}</span>
+                        </li>
+                        <li v-if="hasLinks()">
+                            <span class="text-muted">Страницы</span>
+                            <ul class="links">
+                                <li v-for="link in actor.links"><a :href="link">{{link}}</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </div>`,
