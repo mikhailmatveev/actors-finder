8628028b604652d35bde777b49299bc7fb0fe76e
mikhailmatveev <mikhailmatveev.tmb@gmail.com>
2018-03-02 21:27:35 +0300

    small refactor

--- frontend/src/js/app.js
+++ frontend/src/js/app.js
@@ -2,29 +2,27 @@ import Vue from 'vue';
 import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 
-const router = new VueRouter({
-    routes: [{
-        path: '/',
-        component: resolve => { require(['../../components/actor-search/actor-search.vue'], resolve); }
-    }, {
-        path: '/actor/:id',
-        component: resolve => { require(['../../components/actor-info/actor-info.vue'], resolve); }
-    }, {
-        path: '/404',
-        name: '404',
-        component: {
-            template: '<h1>Not found (404)</h1>'
-        }
-    }, {
-        path: '*',
-        redirect: '404'
-    }]
-});
-
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
 new Vue({
     el: '#app',
-    router
+    router: new VueRouter({
+        routes: [{
+            path: '/',
+            component: resolve => { require(['../../components/actor-search/actor-search.vue'], resolve); }
+        }, {
+            path: '/actor/:id',
+            component: resolve => { require(['../../components/actor-info/actor-info.vue'], resolve); }
+        }, {
+            path: '/404',
+            name: '404',
+            component: {
+                template: '<h1>Not found (404)</h1>'
+            }
+        }, {
+            path: '*',
+            redirect: '404'
+        }]
+    });
 });
